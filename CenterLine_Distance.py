ee0eacfc86d8bb3316ecc0fb7d77932f0a80c53d
John S. Marcial-Manjarres <59844100+jm01146@users.noreply.github.com>
2025-02-27 01:31:02 -0500

    Update CenterLine_Distance.py

--- CenterLine_Distance.py
+++ CenterLine_Distance.py
@@ -1,16 +1,20 @@
+# Importing Libraries
 import serial.tools.list_ports
 import numpy as np
 import cv2
 
+# Setup of the COM port communcation
 ports = serial.tools.list_ports.grep('COM')
 serialInst = serial.Serial()
 
+# Way to list all ports available
 portsList = []
 
 for onePort in ports:
     portsList.append(str(onePort))
     print(str(onePort))
 
+# Choose the port you want to connect to
 val = input('Select Port: COM')
 
 for x in range(0, len(portsList)):
@@ -18,6 +22,7 @@ for x in range(0, len(portsList)):
         portVar = 'COM' + str(val)
         print(portVar)
 
+# Change the baudrate and then it opens the channel
 serialInst.baudrate = 115200
 serialInst.port = portVar
 serialInst.open()
@@ -54,16 +59,15 @@ cap = cv2.VideoCapture(0)
 
 # Use this to get the max width frames to know when to move left and right and by how much.
 default_width = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-default_height = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
+# print("Default frame width:", default_width)  #CALIBRATION PURPOSES ONLY
 
-# print("Default frame width:", default_width)
-# print("Default frame height:", default_height)
 
 # Accesses camera and finds the targeted color and displays it
 while True:
     ret, frame = cap.read()
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     mask = cv2.inRange(hsv, lower_color[2], higher_color[2])
+    # Activate mask 2 only when you are detecting red
     # mask2 = cv2.inRange(hsv, lower_color[1], higher_color[1])
     # mask = mask & mask2
     mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernel)
@@ -72,29 +76,37 @@ while True:
     results = frame.copy()
     results = cv2.drawContours(results, mask_contours, -1, (0, 0, 255), 3)
 
+    # The natural state of command should be 0 in case there is no color detected
+    command = f'0\n'
+
     # crates a box around the targeted color
     if len(mask_contours) != 0:
         for mask_contours in mask_contours:
             if cv2.contourArea(mask_contours) > 500:
+                # This finds the center of the color
                 M = cv2.moments(mask_contours)
                 cX = int(M["m10"] / M["m00"])
                 cY = int(M["m01"] / M["m00"])
                 cv2.circle(results, (cX, cY), 7, (0, 0, 0), -1)
+                # Find the center and the distance from the center
                 movement_variable = cX - (default_width/2)
+                # Sends the distance from the center as a command to the arduino
                 command = f'{movement_variable}\n'
-                serialInst.write(command.encode('utf-8'))
-                print(f'X: {cX}, Y: {cY}')
-                print(f'Move by: {movement_variable}')  # this you will send to the arduino
+                print(f'Move by: {movement_variable}')  #DEGUGGING PURPOSES ONLY
+
 
                 # On the arduino end make it receive these number fi it's a negative take the absolute and move only the
                 # left motors if positive just move the right motors
+
+    # Send the command
+    serialInst.write(command.encode('utf-8'))
 
 # This shows the photo with the box, the mask, and what the computer will see before putting a box this is for debugging
 # purposes
 #     cv2.imshow('Frame', frame)
 #     cv2.imshow('HSV', hsv)
 #     cv2.imshow('Mask', mask)
-    cv2.imshow('Results', results)
+    cv2.imshow('Results', results)  #You need this to close actually
 
 # Waits for the user to hit the q button to close program #
     if cv2.waitKey(1) == ord('q'):
