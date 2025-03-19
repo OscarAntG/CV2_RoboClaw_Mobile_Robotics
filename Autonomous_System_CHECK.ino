f35c72cb3e6f4d05f235f312924505c9b4025488
John S. Marcial-Manjarres <59844100+jm01146@users.noreply.github.com>
2025-03-19 03:09:37 -0400

    Update Autonomous_System_CHECK.ino

--- Autonomous_System_CHECK.ino
+++ Autonomous_System_CHECK.ino
@@ -3,6 +3,11 @@
 // Author John Marcial and Oscar Gonzalez
 #include <RoboClaw.h>
 
+#define defaultSpeedMotor1 96
+#define defaultSpeedMotor2 223
+#define stopMotor1 64
+#define stopMotor2 192
+
 // Initialize RoboClaw object
 RoboClaw roboclaw(&Serial1, 10000);
 
@@ -17,22 +22,22 @@ void loop() {
     // Check if data is available from computer
     if (Serial.available()){
       String data = Serial.readStringUntil('\n');
-      int cx = 0;
-      sscanf(data.c_str(), "%d", &cx);
-      findPosistionfromCenter(cx);
+      int cx, error = 0;
+      sscanf(data.c_str(), "%d,%d", &cx, &error);
+      findPosistionfromCenter(cx, error);
     }
 }
 
-void findPosistionfromCenter(int cx) {
-  if (cx > 48) {
-    Serial1.write(127);  // 127	Channel 1 full forward
-    Serial1.write(192);  // 192	Channel 2 stop
-  } else if (cx < -48) {
-    Serial1.write(128);  // 128	Channel 2 full forward
-    Serial1.write(64);  // 64	Channel 1 stop
-  } else if ( cx != 0 && cx >= -48 && cx <= 48){
-    Serial1.write(127);  // 127	Channel 1 full forward
-    Serial1.write(128);  // 128	Channel 2 full forward
+void findPosistionfromCenter(int cx, int error) {
+  if (cx < -106) {
+    Serial1.write(defaultSpeedMotor1 + error)));  // Foward Motor 1 by a percent amount 
+    Serial1.write(stopMotor2 - error);  // Reverses Motor 2 by a percent amount Stop is 192 if 100% then hits reverse by taking 64 to make 128
+  } else if (cx > 106) {
+    Serial1.write(defaultSpeedMotor2 - error);  // 128	Channel 2 full forward
+    Serial1.write(stopMotor1 - error);  // 64	Channel 1 stop
+  } else if ( cx != 0 && cx >= -106 && cx <= 106){
+    Serial1.write(defaultSpeedMotor1);  // 127	Channel 1 full forward
+    Serial1.write(defaultSpeedMotor2);  // 128	Channel 2 full forward
   } else{
     terminateInactive();
   }
